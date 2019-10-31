e2db2d8754607a1f9e2f2025db1bd88a20a8ee63
may21 <may21.lee@gmail.com>
2019-10-31 15:13:48 +0900

    transfer to integer type

--- ku.stella/Stella-OpenStack.py
+++ ku.stella/Stella-OpenStack.py
@@ -197,7 +197,7 @@ def StellaAPI_Filter():
 
     for index in list_hypervisor_name:
         idle = hypervisors.get_capacity(hypervisors, index, _SLA_option)
-        if _SLA_value < idle:
+        if int(_SLA_value) < idle:
             hypervisors["index"]["_SLA_option"] = idle - int(_SLA_value)
             return index
 
