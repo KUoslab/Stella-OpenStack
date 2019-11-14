57335139ed376955a79b7b8df57a5f55b82e4009
osstarlab <starlab@os.korea.ac.kr>
2019-11-14 10:35:05 +0900

    comment enable logging

--- ku.stella/connect_openstack.py
+++ ku.stella/connect_openstack.py
@@ -13,7 +13,8 @@ import openstack
 from openstack import utils
 from openstack.config import loader
 
-utils.enable_logging(True, stream=sys.stdout)
+# kwlee
+#utils.enable_logging(True, stream=sys.stdout)
 
 #: Defines the OpenStack Config loud key in your config file,
 #: typically in $HOME/.config/openstack/clouds.yaml. That configuration
