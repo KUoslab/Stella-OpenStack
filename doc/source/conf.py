0bc6840fb1f20873a52f8a72899528f4b64e1129
oslab <yrkim@os.korea.ac.kr>
2020-05-28 14:42:41 +0900

    modi

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -11,6 +11,7 @@
 # implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# This work is done by Operating System Lab
 
 import os
 import sys
